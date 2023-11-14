3ad51de5d6c44d95598a79a143ab2b0a54dc7b20
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-11-14 17:35:11 -0300

    Score, delete stone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +10,16 @@ public class GameManager : MonoBehaviour
 
     public float maxX;
 
+    int score = 0;
 
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnStone", 0.5f, 1.1f);
+
+        InvokeRepeating("UpdateScore", 3.8f, 1.1f);
     }
 
     // Update is called once per frame
@@ -29,5 +34,11 @@ public class GameManager : MonoBehaviour
         spawnPos.x = Random.Range(-maxX, maxX);
 
         Instantiate(stone, spawnPos, Quaternion.identity);
+    }
+
+    void UpdateScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
     }
 }
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
